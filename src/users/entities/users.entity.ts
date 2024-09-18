63a5867a9d224e15be7283eb073d18e08cdc9413
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-09-18 20:12:51 +0900

    🚩 Exclude Annotation 사용해보기

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -6,6 +6,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -35,6 +36,21 @@ export class UsersModel extends BaseModel {
     message: stringValidationMessage,
   })
   @Length(3, 8, { message: lengthValidationMessage })
+  /**
+   * Request
+   * frontend -> backend
+   * plain object (JSON) -> class instance (DTO)
+   *
+   * Response
+   * backend -> frontend
+   * class instance (DTO) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance로 변환될 때만
+   * toPlainOnly -> plain object로 변환될 때만
+   */
+  @Exclude({
+    toPlainOnly: true,
+  })
   password: string;
 
   @Column({
